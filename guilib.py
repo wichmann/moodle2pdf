991dceac26305ab3fdc1893230f9487a85e634a8
Christian Wichmann <wichmann@bbs-os-brinkstr.de>
2020-02-27 14:48:38 +0100

    Fixed an issue with PyInstaller check.

--- guilib.py
+++ guilib.py
@@ -13,13 +13,13 @@ def get_resource_path(relative_path):
     """
     Get absolute path to resource, works for dev and for PyInstaller.
 
-    Source: https://stackoverflow.com/a/13790741
+    Based on PyInstaller manual and StackOverflow (https://stackoverflow.com/a/13790741)
     """
     logger.debug('Looking for resource: {}'.format(relative_path))
-    try:
+    if hasattr(sys, 'frozen') and hasattr(sys, '_MEIPASS'):
         # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except AttributeError:
+        base_path = getattr(sys, '_MEIPASS', os.path.abspath(os.path.dirname(__file__)))
+    else:
         base_path = os.path.abspath(".")
     logger.debug('Resource should be at: {}'.format(base_path))
     return os.path.join(base_path, relative_path)
