aaf0327ef50e8c96de10ac1caf275aa4c8415383
Christian Wichmann <wichmann@bbs-os-brinkstr.de>
2020-03-21 11:20:18 +0100

    Added German translation for Moodle2PDF and MoodleEditor.

---
+++ translate/de_DE.ts
@@ -0,0 +1,300 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>CredentialsDialog</name>
+    <message>
+        <location filename="../guilib.py" line="31"/>
+        <source>Enter credentials...</source>
+        <translation>Benutzerdaten eingeben...</translation>
+    </message>
+    <message>
+        <location filename="../guilib.py" line="33"/>
+        <source>Username: </source>
+        <translation>Benutzername: </translation>
+    </message>
+    <message>
+        <location filename="../guilib.py" line="35"/>
+        <source>username</source>
+        <translation>benutzer</translation>
+    </message>
+    <message>
+        <location filename="../guilib.py" line="40"/>
+        <source>Password: </source>
+        <translation>Passwort: </translation>
+    </message>
+    <message>
+        <location filename="../guilib.py" line="42"/>
+        <source>password</source>
+        <translation>passwort</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../moodle2pdf.ui" line="14"/>
+        <source>Moodle2PDF</source>
+        <translation>Moodle2PDF</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.ui" line="34"/>
+        <source>Activities and Materials</source>
+        <translation>Aktivitäten und Materialien</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.ui" line="44"/>
+        <source>Moodle Site</source>
+        <translation>Moodle-Seite</translation>
+    </message>
+    <message>
+        <location filename="../moodle2pdf.ui" line="50"/>
+        <source>Export</source>
+        <translation>Exportieren</translation>
+    </message>
+    <message>
+        <location filename="../moodle2pdf.ui" line="63"/>
+        <source>Combine all glossaries into one PDF file</source>
+        <translation>Alle Module in eine gemeinsame PDF-Datei ausgeben</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.ui" line="63"/>
+        <source>File</source>
+        <translation>Datei</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.ui" line="71"/>
+        <source>Help</source>
+        <translation>Hilfe</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.ui" line="84"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.ui" line="87"/>
+        <source>Ctrl+O</source>
+        <translation>Strg+O</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.ui" line="92"/>
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.ui" line="95"/>
+        <source>Ctrl+H</source>
+        <translation>Strg+H</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.ui" line="100"/>
+        <source>Quit</source>
+        <translation>Beenden</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.ui" line="103"/>
+        <source>Ctrl+Q</source>
+        <translation>Strg+Q</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.ui" line="108"/>
+        <source>Set Site...</source>
+        <translation>Server-Adresse setzen...</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.ui" line="111"/>
+        <source>Ctrl+S</source>
+        <translation>Strg+S</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.ui" line="20"/>
+        <source>MoodleEditor</source>
+        <translation>MoodleEditor</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.ui" line="39"/>
+        <source>Visibility</source>
+        <translation>Sichtbarkeit</translation>
+    </message>
+</context>
+<context>
+    <name>Moodle2PdfWindow</name>
+    <message>
+        <location filename="../moodle2pdf_gui.py" line="56"/>
+        <source>Moodle Site ({})</source>
+        <translation>Moodle-Seite ({})</translation>
+    </message>
+    <message>
+        <location filename="../moodle2pdf_gui.py" line="107"/>
+        <source>Logging in to Moodle site...</source>
+        <translation>Einloggen in Moodle-Seite...</translation>
+    </message>
+    <message>
+        <location filename="../moodle2pdf_gui.py" line="109"/>
+        <source>Enter site URL</source>
+        <translation>Server-Adresse eingeben</translation>
+    </message>
+    <message>
+        <location filename="../moodle2pdf_gui.py" line="109"/>
+        <source>Site URL:</source>
+        <translation>Server-Adresse:</translation>
+    </message>
+    <message>
+        <location filename="../moodle2pdf_gui.py" line="182"/>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <location filename="../moodle2pdf_gui.py" line="120"/>
+        <source>Wrong site URL or credentials.</source>
+        <translation>Falsche Adresse oder Benutzerdaten.</translation>
+    </message>
+    <message>
+        <location filename="../moodle2pdf_gui.py" line="121"/>
+        <source>Logged in.</source>
+        <translation>Eingeloggt.</translation>
+    </message>
+    <message>
+        <location filename="../moodle2pdf_gui.py" line="124"/>
+        <source>Loading all glossaries for course...</source>
+        <translation>Lade alle Glossare aus Kurs...</translation>
+    </message>
+    <message>
+        <location filename="../moodle2pdf_gui.py" line="133"/>
+        <source>All glossaries loaded.</source>
+        <translation>Alle Glossare geladen.</translation>
+    </message>
+    <message>
+        <location filename="../moodle2pdf_gui.py" line="137"/>
+        <source>Loading all wikis for course...</source>
+        <translation>Lade alle Wikis aus Kurs...</translation>
+    </message>
+    <message>
+        <location filename="../moodle2pdf_gui.py" line="146"/>
+        <source>All wikis loaded.</source>
+        <translation>Alle Wikis geladen.</translation>
+    </message>
+    <message>
+        <location filename="../moodle2pdf_gui.py" line="166"/>
+        <source>Save as PDF File...</source>
+        <translation>Speichere als PDF-Datei...</translation>
+    </message>
+    <message>
+        <location filename="../moodle2pdf_gui.py" line="166"/>
+        <source>PDF File (*.pdf)</source>
+        <translation>PDF-Datei (*.pdf)</translation>
+    </message>
+    <message>
+        <location filename="../moodle2pdf_gui.py" line="170"/>
+        <source>Building PDF file...</source>
+        <translation>Baue PDF-Datei...</translation>
+    </message>
+    <message>
+        <location filename="../moodle2pdf_gui.py" line="177"/>
+        <source>Finished PDF file.</source>
+        <translation>PDF-Datei erstellt.</translation>
+    </message>
+    <message>
+        <location filename="../moodle2pdf_gui.py" line="180"/>
+        <source>Separate export is not implemented yet.</source>
+        <translation>Export in einzelne Dateien ist noch nicht implementiert.</translation>
+    </message>
+    <message>
+        <location filename="../moodle2pdf_gui.py" line="182"/>
+        <source>You have to select some modules first.</source>
+        <translation>Bitte zuerst ein oder mehrere Module auswählen.</translation>
+    </message>
+    <message>
+        <location filename="../moodle2pdf_gui.py" line="185"/>
+        <source>About...</source>
+        <translation>Über...</translation>
+    </message>
+    <message>
+        <location filename="../moodle2pdf_gui.py" line="185"/>
+        <source>Moodle2PDF from Christian Wichmann
+Source: https://github.com/wichmann/moodle2pdf</source>
+        <translation>Moodle2PDF von Christian Wichmann
+Quellcode: https://github.com/wichmann/moodle2pdf</translation>
+    </message>
+</context>
+<context>
+    <name>MoodleEditor</name>
+    <message>
+        <location filename="../moodleeditor.py" line="45"/>
+        <source>MoodleEditor</source>
+        <translation>MoodleEditor</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.py" line="72"/>
+        <source>Changing visibility for section...</source>
+        <translation>Sichtbarkeit des Abschnitts ändern...</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.py" line="87"/>
+        <source>Changing visibility for module...</source>
+        <translation>Sichtbarkeit des Moduls ändern...</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.py" line="91"/>
+        <source>Visibility changed.</source>
+        <translation>Sichtbarkeit geändert.</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.py" line="97"/>
+        <source>Moodle Site ({})</source>
+        <translation>Moodle-Seite ({})</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.py" line="114"/>
+        <source>Logging in to Moodle site...</source>
+        <translation>Einloggen in Moodle-Seite...</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.py" line="116"/>
+        <source>Enter site URL</source>
+        <translation>Server-Adresse eingeben</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.py" line="116"/>
+        <source>Site URL:</source>
+        <translation>Server-Adresse:</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.py" line="127"/>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.py" line="127"/>
+        <source>Wrong site URL or credentials.</source>
+        <translation>Falsche Adresse oder Benutzerdaten.</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.py" line="129"/>
+        <source>Logged in.</source>
+        <translation>Eingeloggt.</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.py" line="161"/>
+        <source>Loading all modules for course...</source>
+        <translation>Lade alle Module aus Kurs...</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.py" line="177"/>
+        <source>All modules loaded.</source>
+        <translation>Alle Module geladen.</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.py" line="212"/>
+        <source>About...</source>
+        <translation>Über...</translation>
+    </message>
+    <message>
+        <location filename="../moodleeditor.py" line="212"/>
+        <source>MoodleEditor from Christian Wichmann
+Source: https://github.com/wichmann/moodle2pdf</source>
+        <translation>MoodleEditor von Christian Wichmann
+Quellcode: https://github.com/wichmann/moodle2pdf</translation>
+    </message>
+</context>
+</TS>
